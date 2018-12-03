92390eadfb2dd1ef0bc9739e3373bfc6fcd27f20
Gaurang Omar <gaurang.omar@geminisolutions.in>
2018-12-03 14:25:47 +0530

    Queue

---
+++ src/queue.js
@@ -0,0 +1,35 @@
+function Queue() {
+	let queue = []
+	return {
+		enqueue(item) {
+			return queue.push(item)
+		},
+		dequeue() {
+			return queue.shift() || null;
+		},
+		get length() {
+			return queue.length;
+		},
+		peek() {
+			return queue[0];
+		}
+	}
+}
+
+let q = new Queue();
+q.enqueue('Gaurang');
+q.enqueue('Omar')
+q.enqueue('Smart')
+console.log(q.length)
+q.enqueue('Man')
+console.log(q.length)
+console.log(q.dequeue())
+console.log(q.dequeue())
+console.log(q.peek())
+console.log(q.dequeue())
+console.log(q.peek())
+console.log(q.dequeue())
+console.log(q.dequeue())
+q.enqueue('Yooo')
+console.log(q.length)
+console.log(q.peek())
